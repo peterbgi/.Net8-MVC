63598bb648bf591e5a8eff20b11402e072ec6d0c
Peter Bagi <peti.bagi@icloud.com>
2024-06-29 16:20:45 +0200

    modified methods

--- Shop.Web/Areas/Admin/Controllers/ProductController.cs
+++ Shop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -25,7 +25,7 @@ namespace Shop.Web.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Add()
+        public IActionResult Upsert(int? id)
         {
             ProductViewModel viewModel = new()
             {
@@ -36,12 +36,21 @@ namespace Shop.Web.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
+            if (id == null || id == 0)
+            {     //create
+                return View(viewModel);
+            }
+            else
+            {     //update
+                viewModel.Product = _unitOfWork.Product.Get(x => x.Id == id);
+                return View(viewModel);
+            }
 
-            return View(viewModel);
+            
         }
 
         [HttpPost]
-        public IActionResult Add(ProductViewModel productModel)
+        public IActionResult Upsert(ProductViewModel productModel, IFormFile file)
         {
 
             if (ModelState.IsValid)
@@ -65,39 +74,7 @@ namespace Shop.Web.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFormDb = _unitOfWork.Product.Get(x => x.Id == id);
-
-            //Product? categoryFormDb2 = _db.Categories.Where(x => x.Id == id).FirstOrDefault(); 
-
-            if (productFormDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFormDb);
-
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Kategoria sikeresen frissítve";
-                return RedirectToAction("Index");
-            }
-            return View();
-
-        }
+       
 
         public IActionResult Delete(int? id)
         {
